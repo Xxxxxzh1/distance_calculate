05fc870a5fc417c6ec28c9ae3c0967520f0b1a5d
Xxxxxzh1 <17551018764@163.com>
2022-06-22 15:56:52 +0800

    done

--- gaode/gaode_map_api.py
+++ gaode/gaode_map_api.py
@@ -1,6 +1,7 @@
 import os
 from typing import Optional
 import sys
+import csv
 
 import pandas as pd
 import requests
@@ -21,7 +22,7 @@ class GaodeMap:
     使用该类处理所有有高德地图API的操作
     """
     def __init__(self):
-        self.directionV1_url = 'http://restapi.amap.com/v3/direction/driving'
+        self.directionV1_url = 'https://restapi.amap.com/v3/direction/driving'
         self.directionV2_url = 'http://restapi.amap.com/v5/direction/driving'
 
         self.session = requests.Session()
@@ -30,20 +31,25 @@ class GaodeMap:
         """
         使用高德地图API需要提供key
 
-        @param ak: 从高德地图API的控制台处获得的key
+        @param key: 从高德地图API的控制台处获得的key
         """
         self.key = key
 
 
-    def get_cities_lng_lat(self, file_path_cities: str, file_path_cities_lng_lat: str, key: str) -> None:
+    def get_cities_lng_lat(self, file_path_cities: str) -> None:
         """
         使用高德地图API需要提供key
-        @param file_path_cities: 城市数据存储路径
-        @param ak: 从高德地图API的控制台处获得的key
+        @param file_path_cities: 城市名称csv存储路径
+        @param file_path_cities_lng_lat: 城市经纬度csv存储路径
+        @param key: 从高德地图API的控制台处获得的key
         """
-        self.key = key
         cities_df = read_df(file_path_cities)
-        cities_lng_lat_df = read_df(file_path_cities_lng_lat)
+        cities_lng_lat_dict = {
+            'city':[],
+            'longitude':[],
+            'latitude':[]
+        }
+
         for index in range(len(cities_df)):
             params = {
                 'key': self.key,
@@ -53,12 +59,44 @@ class GaodeMap:
 
             resp = self.session.get(url = 'http://restapi.amap.com/v3/geocode/geo', params = params).json()
             if resp['status'] == '1':
-                cities_lng_lat_df[cities_df.at[index, 'city']] = (round(resp['geocodes'][0]['location'].split(',')[0], 5), round(resp['geocodes'][0]['location'].split(',')[0], 5))
+                cities_lng_lat_dict['city'].append(cities_df.at[index, 'city'])
+                cities_lng_lat_dict['longitude'].append(resp['geocodes'][0]['location'].split(',')[0])
+                cities_lng_lat_dict['latitude'].append(resp['geocodes'][0]['location'].split(',')[1])
             else:
                 print(params)
                 raise Exception('调用出错！')
+
+        cities_lng_lat_df = pd.DataFrame(cities_lng_lat_dict)
         cities_lng_lat_df.to_csv('cities_lng_lat_gaode.csv', index=False)
-            
+
+    def init_result_csv(self, file_path: str) -> None:
+        """
+        使用高德地图API需要提供key
+        @param file_path: 城市名称csv存储路径
+        """
+
+        cities_df = read_df(file_path)
+        cities_list = cities_df.values
+        init_result_dict = {
+            'origin': [],
+            'destination': [],
+            'gaode_map_distance': [],
+            'duration': []
+        }
+
+        for i in range(len(cities_list)):
+            for city in cities_list:
+                if cities_list[i] != city :
+                    init_result_dict['origin'].append(cities_list[i][0])
+                    init_result_dict['destination'].append(city[0])
+                    init_result_dict['gaode_map_distance'].append(0)
+                    init_result_dict['duration'].append(0)
+                else:
+                    print(f"{i}目的地起始地相同")
+
+        cities_lng_lat_df = pd.DataFrame(init_result_dict)
+        cities_lng_lat_df.to_csv('distance_gaode.csv', index=False)        
+
 
     def read_lng_lat(self, file_path: str) -> None:
         """
@@ -96,8 +134,8 @@ class GaodeMap:
             url = self.directionV2_url
         
         params = {
-            'origin': f'{origin[1]},{origin[0]}',
-            'destination': f'{destination[1]},{destination[0]}',
+            'origin': f'{origin[0]},{origin[1]}',
+            'destination': f'{destination[0]},{destination[1]}',
             'key': self.key,
             'extensions': 'all'
         }
@@ -107,9 +145,8 @@ class GaodeMap:
         if resp['status'] == '1':
             routes = resp['route']['paths'][0]
             return {
-                'distance': routes['distance'] / 1000,
-                'duration': routes['duration'],
-                'toll': routes['toll'],
+                'distance': routes['distance'],
+                'duration': routes['duration']
             }
         else:
             print(params)
@@ -118,7 +155,10 @@ class GaodeMap:
 if __name__ == "__main__":
     import time
     map = GaodeMap()
-    map.login('please enter your key')
+    map.login('673f63b7ea3b20ff3bf73cbf2c9db60f')
+    # 刷新列表城市的经纬度
+    map.get_cities_lng_lat('cities.csv')
+    map.init_result_csv('cities.csv')
     map.read_lng_lat('cities_lng_lat_gaode.csv')
     
     result_df = read_df('distance_gaode.csv')
@@ -144,7 +184,6 @@ if __name__ == "__main__":
             
             result_df.at[index, 'gaode_map_distance'] = resp['distance']
             result_df.at[index, 'duration'] = resp['duration']
-            result_df.at[index, 'toll'] = resp['toll']
 
             if index % 20 == 19:
                 print(f'处理到第 {index + 1} 条数据，准备存盘...')
